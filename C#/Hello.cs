e3ce398eebc5d9b5c73df52a39a36a53e390a5e3
Sema Parlak <101374763+semaparlak@users.noreply.github.com>
2022-03-11 05:23:54 +0300

    Add files via upload

---
+++ C#/Hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Sema
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello may name is Sema");
+        }
+    }
+}
---
+++ C++/Hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Hello my name is Sema";
+    return 0;
+}
